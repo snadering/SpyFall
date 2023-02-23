a3a18bc585da2f33f39fac0e033406d028a69093
snadering <cph-sr319@cphbusiness.dk>
2023-02-23 15:36:26 +0100

    Colors added. Done

--- src/MainMenu.java
+++ src/MainMenu.java
@@ -14,28 +14,28 @@ public class MainMenu {
         String input = TextUI.getStringInput("Pick the number corresponding to you and press 'ENTER'");
         TextUI.clearConsole();
         if (input.trim().equals("1")){
-            TextUI.displayMessage("Name: " + players.get(0).getName());
-            TextUI.displayMessage("Role: " + players.get(0).getRole());
-            TextUI.displayMessage("Location: " + players.get(0).getLocation());
+            TextUI.displayMessage(Color.ANSI_BLACK_BACKGROUND + " Name: " + players.get(0).getName() + " " + Color.ANSI_RESET);
+            TextUI.displayMessage(Color.ANSI_BLACK_BACKGROUND + " Role: " + players.get(0).getRole() + " " + Color.ANSI_RESET);
+            TextUI.displayMessage(Color.ANSI_BLACK_BACKGROUND + " Location: " + Color.ANSI_RESET + players.get(0).getLocation());
         } else if (input.trim().equals("2")){
-            TextUI.displayMessage("Name: " + players.get(1).getName());
-            TextUI.displayMessage("Role: " + players.get(1).getRole());
-            TextUI.displayMessage("Location: " + players.get(1).getLocation());
+            TextUI.displayMessage(Color.ANSI_BLACK_BACKGROUND + " Name: " + players.get(1).getName() + " " + Color.ANSI_RESET);
+            TextUI.displayMessage(Color.ANSI_BLACK_BACKGROUND + " Role: " + players.get(1).getRole() + " " + Color.ANSI_RESET);
+            TextUI.displayMessage(Color.ANSI_BLACK_BACKGROUND + " Location: " + Color.ANSI_RESET + players.get(1).getLocation());
         } else if (input.trim().equals("3")){
-            TextUI.displayMessage("Name: " + players.get(2).getName());
-            TextUI.displayMessage("Role: " + players.get(2).getRole());
-            TextUI.displayMessage("Location: " + players.get(2).getLocation());
+            TextUI.displayMessage(Color.ANSI_BLACK_BACKGROUND + " Name: " + players.get(2).getName() + " " + Color.ANSI_RESET);
+            TextUI.displayMessage(Color.ANSI_BLACK_BACKGROUND + " Role: " + players.get(2).getRole() + " " + Color.ANSI_RESET);
+            TextUI.displayMessage(Color.ANSI_BLACK_BACKGROUND + " Location: " + Color.ANSI_RESET + players.get(2).getLocation());
         } else if (input.trim().equals("4")){
-            TextUI.displayMessage("Name: " + players.get(3).getName());
-            TextUI.displayMessage("Role: " + players.get(3).getRole());
-            TextUI.displayMessage("Location: " + players.get(3).getLocation());
+            TextUI.displayMessage(Color.ANSI_BLACK_BACKGROUND + " Name: " + players.get(3).getName() + " " + Color.ANSI_RESET);
+            TextUI.displayMessage(Color.ANSI_BLACK_BACKGROUND + " Role: " + players.get(3).getRole() + " " + Color.ANSI_RESET);
+            TextUI.displayMessage(Color.ANSI_BLACK_BACKGROUND + " Location: " + Color.ANSI_RESET + players.get(3).getLocation());
         } else if (input.trim().equals("5")){
-            TextUI.displayMessage("Name: " + players.get(4).getName());
-            TextUI.displayMessage("Role: " + players.get(4).getRole());
-            TextUI.displayMessage("Location: " + players.get(4).getLocation());
+            TextUI.displayMessage(Color.ANSI_BLACK_BACKGROUND + " Name: " + players.get(4).getName() + " " + Color.ANSI_RESET);
+            TextUI.displayMessage(Color.ANSI_BLACK_BACKGROUND + " Role: " + players.get(4).getRole() + " " + Color.ANSI_RESET);
+            TextUI.displayMessage(Color.ANSI_BLACK_BACKGROUND + " Location: " + Color.ANSI_RESET + players.get(4).getLocation());
         }
         TextUI.displayMessage("");
-        TextUI.getStringInput("Press 'B' to go back.");
+        TextUI.getStringInput(Color.ANSI_BLACK_BACKGROUND + "Press 'B' to go back." + Color.ANSI_RESET);
         infoMenu(players);
 
     }
--- src/StartMenu.java
+++ src/StartMenu.java
@@ -16,11 +16,6 @@ public class StartMenu {
 
         listOfPlayers = new ArrayList<>();
         createPlayerNames(amountOfPlayers());
-
-        //Test printing all the player names.
-        for (Player p : listOfPlayers){
-            System.out.println(p);
-        }
      return listOfPlayers;
     }
 
@@ -33,8 +28,8 @@ public class StartMenu {
         int spy = rnd.nextInt(amountOfPlayers);
         //spy++;
         players.get(spy).setSpy(true);
-        players.get(spy).setRole("The Spy");
-        players.get(spy).setLocation("???");
+        players.get(spy).setRole(Color.ANSI_BLACK_BACKGROUND + Color.ANSI_RED + "The Spy " + Color.ANSI_RESET);
+        players.get(spy).setLocation(Color.ANSI_BLACK_BACKGROUND + Color.ANSI_RED + "??? " + Color.ANSI_RESET);
 
         //Putting the location in a variable, to make sure all players get the same location.
         String location = DatabaseIO.getLocation(locationID);
@@ -42,76 +37,70 @@ public class StartMenu {
 
         for (Player p : players){
             if (!p.isSpy()){
-                p.setLocation(location);
+                p.setLocation(Color.ANSI_BLACK_BACKGROUND + Color.ANSI_BLUE + location + " " + Color.ANSI_RESET);
             }
         }
         ArrayList<String> roles = DatabaseIO.getRoles(locationID);
         int iterator = 0;
         for (Player p : players) {
             if (!p.isSpy()){
-                p.setRole(roles.get(iterator++));
+                p.setRole(Color.ANSI_YELLOW + roles.get(iterator++) + " " +  Color.ANSI_RESET);
             }
         }
-        //TEST - Start -
-        for (Player p : players){
-            System.out.println(p);
-        }
-        //TEST - End -
     }
 
     private int amountOfPlayers(){
         //Initializing the string, but assigning a key to know if the user has changed the input.
         String amount = "1234567890987654>Z>Z32123456789098765432123456789098765432";
-        TextUI.displayMessage("Welcome to SpyFall");
+        TextUI.displayMessage(Color.ANSI_BLACK_BACKGROUND + "Welcome to " + Color.ANSI_BLACK + Color.ANSI_WHITE_BACKGROUND + " SpyFall " + Color.ANSI_RESET);
         do {
             if (!amount.equals("1234567890987654>Z>Z32123456789098765432123456789098765432")){
                 TextUI.clearConsole();
-                TextUI.displayMessage("Sorry '" + amount + "' isn't a valid input");
+                TextUI.displayMessage("Sorry '"+ Color.ANSI_RED + amount + Color.ANSI_RESET + "' isn't a valid input");
             }
-        TextUI.displayMessage("First select how many players will be playing (3-5 Players).");
-         amount = TextUI.getStringInput("Amount of players: ");
+        TextUI.displayMessage(Color.ANSI_BLACK_BACKGROUND + "First select how many players will be playing (3-5 Players)." + Color.ANSI_RESET);
+         amount = TextUI.getStringInput(Color.ANSI_BLACK_BACKGROUND + "Amount of players: " + Color.ANSI_RESET);
         } while (!amount.equals("3") && !amount.equals("4") && !amount.equals("5"));
 
         switch (amount) {
             case "3":
-                TextUI.displayMessage("You have successfully chosen 3 players.");
+                TextUI.displayMessage(Color.ANSI_BLACK_BACKGROUND + Color.ANSI_GREEN + "You have successfully chosen 3 players." + Color.ANSI_RESET);
                 playerAmount = 3;
                 return 3;
             case "4":
-                TextUI.displayMessage("You have successfully chosen 4 players.");
+                TextUI.displayMessage(Color.ANSI_BLACK_BACKGROUND + Color.ANSI_GREEN + "You have successfully chosen 4 players." + Color.ANSI_RESET);
                 playerAmount = 4;
                 return 4;
             case "5":
-                TextUI.displayMessage("You have successfully chosen 5 players.");
+                TextUI.displayMessage(Color.ANSI_BLACK_BACKGROUND + Color.ANSI_GREEN + "You have successfully chosen 5 players." + Color.ANSI_RESET);
                  playerAmount = 5;
                 return 5;
 
         }
-        System.out.println("This is not supposed to happen. (amountOfPlayers)");
         return -1;
     }
 
     private ArrayList<Player> createPlayerNames(int amountOfPlayers){
-        String currentName = TextUI.getStringInput("Please enter the name of Player 1: ");
+        String currentName = TextUI.getStringInput(Color.ANSI_BLACK_BACKGROUND + "Please enter the name of Player 1: " + Color.ANSI_RESET);
         Player player1 = new Player(currentName, false, "", "");
         listOfPlayers.add(player1);
         if (amountOfPlayers > 1){
-            currentName = TextUI.getStringInput("Please enter the name of Player 2: ");
+            currentName = TextUI.getStringInput(Color.ANSI_BLACK_BACKGROUND + "Please enter the name of Player 2: " + Color.ANSI_RESET);
             Player player2 = new Player(currentName, false, "", "");
             listOfPlayers.add(player2);
         }
         if (amountOfPlayers > 2) {
-            currentName = TextUI.getStringInput("Please enter the name of Player 3: ");
+            currentName = TextUI.getStringInput(Color.ANSI_BLACK_BACKGROUND + "Please enter the name of Player 3: " + Color.ANSI_RESET);
             Player player3 = new Player(currentName, false, "", "");
             listOfPlayers.add(player3);
         }
         if (amountOfPlayers > 3) {
-            currentName = TextUI.getStringInput("Please enter the name of Player 4: ");
+            currentName = TextUI.getStringInput(Color.ANSI_BLACK_BACKGROUND + "Please enter the name of Player 4: " + Color.ANSI_RESET);
             Player player4 = new Player(currentName, false, "", "");
             listOfPlayers.add(player4);
         }
         if (amountOfPlayers > 4) {
-            currentName = TextUI.getStringInput("Please enter the name of Player 5: ");
+            currentName = TextUI.getStringInput(Color.ANSI_BLACK_BACKGROUND + "Please enter the name of Player 5: " + Color.ANSI_RESET);
             Player player5 = new Player(currentName, false, "", "");
             listOfPlayers.add(player5);
         }
